8242ccb5ae5794450220bbbbd57dd7668587736d
Alex Grintsvayg <alex@flickswitch.cc>
2011-09-23 09:35:35 -0400

    is this the right url?

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-
 var cradle = require('cradle');
 
 this.connection = new(cradle.Connection)({
 //    host:  '127.0.0.1',
     host: process.env['CLOUDANT_URL'],
-    port:  5984
+    port: 443 //5984
 });
 
 this.database = this.connection.database('thingler');

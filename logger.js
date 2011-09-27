2b23233bd5f8bdb21e555571856884f123aefbf5
Alex Grintsvayg <alex@flickswitch.cc>
2011-09-27 18:51:31 -0400

    testing loggly

--- logger.js
+++ logger.js
@@ -2,10 +2,8 @@ var loggly = require('loggly')
 var config = { subdomain: "lyoshenka" };
 var client = loggly.createClient(config);
 var defaultInputToken = '79852208-a320-4202-8f9c-cbfcd7ee05b6';
-var sys = require('sys');
 
 this.log = function (message) 
 {
-  sys.puts('Logging message ' + message);
   client.log(defaultInputToken, message);
 };
